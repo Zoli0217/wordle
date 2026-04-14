30d3e6e03bfc44d96c992fe1ffe29431f4269804
Zoli0217 <zoltanbodi61@gmail.com>
2026-04-14 13:56:24 +0200

    colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React, { useState } from 'react'
-import { sendGuess } from './services/api'
+import { sendGuess, type ApiData } from './services/api'
 
 const App = () => {
   const [guess, setGuess] = useState<string>("")
+  const [history, setHistory] = useState<ApiData[]>([]) 
 
   const submitGuessForm = (e:React.SubmitEvent) => {
     e.preventDefault()
     if(guess.length == 5){
-      sendGuess(guess).then(response => console.log(response))
+      sendGuess(guess).then(response => {
+        if(!response.is_word_in_list){
+          alert("Nincs ilyen szó!")
+        }else{
+          setHistory(prev => [...prev, response])
+        }
+      })
     }
   }
-
 
   return (
     <div>
@@ -18,6 +24,13 @@ const App = () => {
         <input type="text" value={guess} onChange={(e)=>setGuess(e.target.value)} />
         <button type='submit'>BEKULD</button>
       </form>
+
+      {history.map(item => <h1>
+        {item.character_info.map(char => 
+        <span style={{ color: char.scoring.correct_idx ? "green" : char.scoring.in_word ? "yellow" : "white" }}>
+          {char.char}
+          </span>)}
+      </h1>)}
       
     </div>
   )
